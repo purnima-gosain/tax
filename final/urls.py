93d893775ae1ba486f234794492607b11ec55061
purnima-poo <np03a190173@herladcollege.edu.np>
2021-04-14 22:27:20 +0545

    add jinja template

--- final/urls.py
+++ final/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='index.html'),
-    path('POST',views.post , name="post")
+    path('POST',views.post , name="tax")
 ]
--- final/views.py
+++ final/views.py
@@ -5,9 +5,21 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'index.html')
 def post(request):
-    sal= request.GET['salary']
-    sal2= sal
-
-    
-
-    return render(request,'tax.html',{'tax':sal2})
+    sal= request.GET['salary'] 
+    bon=request.GET['Bonus']
+    allow=request.GET['allowance']
+    oth=request.GET['others']
+    total_salary= sal+bon+allow+oth
+# logic to calculate tax according to annual income.
+    if total_salary<=400000:
+        total_tax= total_salary*(0.01)
+    elif total_salary > 400000 and total_salary <=500000:
+        total_tax = total_salary*(0.1)
+    elif total_salary > 500000 and total_salary <=700000:
+        total_tax = total_salary*(0.2)
+    elif total_salary > 700000 and total_salary <=2000000:
+        total_tax = total_salary*(0.3)
+    else:
+        total_tax= total_salary*(0.36)
+ 
+    return render(request,'tax.html',{'tax':total_tax})
